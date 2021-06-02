de6d741869dc6947e068a896d5994264febbbbca
ultrawide <colinchan02@gmail.com>
2021-06-01 23:56:07 -0700

    Bind title and body form to onchange event

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -4,8 +4,15 @@ class PostForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      title: '',
+      body: ''
     };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({[e.target.name]: e.target.value});
   }
 
   render() {
@@ -15,12 +22,14 @@ class PostForm extends Component {
         <form>
           <div>
             <label>Title: </label><br />
-            <input type="text" name="title"/>
+            <input type="text" name="title" onChange={this.onChange} value=
+            {this.state.title} />
           </div>
           <br />
           <div>
             <label>Body: </label><br />
-            <textarea name="body" />
+            <textarea name="body" onChange={this.onChange} value=
+            {this.state.body}/>
           </div>
           <br />
           <button type="submit">Submit</button>
